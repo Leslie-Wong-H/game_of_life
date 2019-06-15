8c5693ccaac0cfb887a415c65c6b88514f4a493b
Leslie Wong <79917148leslie@gmail.com>
2019-06-15 16:45:37 +0800

    （黄崇鹏）修改判断周围细胞数模块v2

--- main/main.js
+++ main/main.js
@@ -1,7 +1,6 @@
 // module.exports = function
 function gameOfLife(matrix) {
     var i = 0, j = 0;
-    var k = 0, p = 0;
     var nLive = 0;
     var nAliveCnt = 0;
     // var nContinue = 0;
@@ -10,19 +9,21 @@ function gameOfLife(matrix) {
     var matrixColumn = matrix[0].length;
 
     var copyMatrix = new Array();
-    for (k = 0; k < matrixRow; k++){
-        copyMatrix[k] = new Array();
+    for (i = 0; i < matrixRow; i++){
+        copyMatrix[i] = new Array();
 
-        for (p = 0; p < matrixColumn; p++){
-            if (matrix[k][p] == 1) {
+        for (j = 0; j < matrixColumn; j++){
+            if (matrix[i][j] == 1) {
                 nLive = nLive+1;
             }
-            copyMatrix[k][p] = matrix[k][p];
+            copyMatrix[i][j] = matrix[i][j];
         }
     }
 
 
     var temp = 0;
+    var anotherTemp = 0;
+
     for (i = 0; i < matrixRow; i++)
     {
         for (j = 0; j < matrixColumn; j++)
@@ -30,55 +31,56 @@ function gameOfLife(matrix) {
             nAliveCnt = 0;  //周围细胞数置零
 
             //判断左上角细胞状态
-            if ((i  > 1) && (j  > 1) && (matrix[i - 1][j - 1] = 1))
+            if ((i >= 1) && (j >= 1) && (matrix[i - 1][j - 1] > 0))
             {
                 nAliveCnt++;
             }
 
             //判断上方细胞状态                
-            if ((i  > 1) && (matrix[i - 1][j] > 0))
+            if ((i  >= 1) && (matrix[i - 1][j] > 0))
             {
                 nAliveCnt++;
             }
 
 
             //判断右上角细胞状态
-            var temp = matrixColumn - 1;
-            if ((j < temp) && (i  > 1) && (matrix[i - 1][j + 1] > 0))
+            temp = matrixColumn - 1;
+            if ((j <= temp) && (i  >= 1) && (matrix[i - 1][j + 1] > 0))
             {
                 nAliveCnt++;
             }
 
             //判断左细胞状态
-            if ((j > 1) && (matrix[i][j - 1] > 0))
+            if ((j >= 1) && (matrix[i][j - 1] > 0))
             {
                 nAliveCnt++;
             }
 
             //判断右细胞状态
             temp = matrixColumn - 1;
-            if ((j < temp) && (matrix[i][j + 1] > 0))
+            if ((j <= temp) && (matrix[i][j + 1] > 0))
             {
                 nAliveCnt++;
             }
 
             //判断左下角细胞状态
             temp = matrixRow - 1;
-            if ((i < matrixRow -1) && (j > 1) && (matrix[i + 1][j - 1] > 0))
+            if ((i <= temp) && (j >= 1) && (matrix[i - 1][j - 1] > 0))
             {
                 nAliveCnt++;
             }
 
             // 判断下方细胞状态
             temp = matrixRow - 1;
-            if ((i < temp) && (matrix[i + 1][j - 1] > 0))
+            if ((i <= temp)  && (matrix[i + 1][j] > 0))
             {
                 nAliveCnt++;
             }
 
             // 判断右下角细胞状态
             temp = matrixRow - 1;
-            if ((i < temp) && (j > 1) && (matrix[i + 1][j - 1] > 0))
+            anotherTemp = matrixColumn - 1;
+            if ((i <= temp) && (j <= anotherTemp) && (matrix[i + 1][j + 1] > 0))
             {
                 nAliveCnt++;
             }
@@ -86,29 +88,29 @@ function gameOfLife(matrix) {
 
             console.log(nAliveCnt);
 
-            // 判断细胞下一代死活情况
-            if (matrix[i][j] == 1)
-            {
-                if ((nAliveCnt == 2) || (nAliveCnt == 3))
-                {
-                    copyMatrix[i][j] = 1;
-                }
-                else
-                {
-                    copyMatrix[i][j] = 0;
-                    nLive--;
-                }
-            }
+            // // 判断细胞下一代死活情况
+            // if (matrix[i][j] == 1)
+            // {
+            //     if ((nAliveCnt == 2) || (nAliveCnt == 3))
+            //     {
+            //         copyMatrix[i][j] = 1;
+            //     }
+            //     else
+            //     {
+            //         copyMatrix[i][j] = 0;
+            //         nLive--;
+            //     }
+            // }
 
 
-            if (matrix[i][j] == 0)
-            {
-                if (nAliveCnt == 3)
-                {
-                    copyMatrix[i][j] = 1;
-                    nLive++;
-                }
-            }
+            // if (matrix[i][j] == 0)
+            // {
+            //     if (nAliveCnt == 3)
+            //     {
+            //         copyMatrix[i][j] = 1;
+            //         nLive++;
+            //     }
+            // }
 
     
         }
