42d945a3d1ecc55204065d8a41874cfa79f2baad
Leslie Wong <79917148leslie@gmail.com>
2019-06-15 17:42:30 +0800

    （黄崇鹏）成功修正九宫格模块

--- main/main.js
+++ main/main.js
@@ -26,112 +26,126 @@ function gameOfLife(matrix) {
 
     for (i = 0; i < matrixRow; i++)
     {
+
+        // 判断细胞九宫格
         for (j = 0; j < matrixColumn; j++)
         {
             nAliveCnt = 0;  //周围细胞数置零
 
             //判断左上角细胞状态
-            if ((i >= 1) && (j >= 1) && (matrix[i - 1][j - 1] > 0))
+            if ((i -1 >= 0) && (j -1 >= 0) && (matrix[i - 1][j - 1] == 1))
+            
             {
                 nAliveCnt++;
             }
 
             //判断上方细胞状态                
-            if ((i  >= 1) && (matrix[i - 1][j] > 0))
+            if ((i - 1 >= 0) && (matrix[i - 1][j] == 1))
             {
                 nAliveCnt++;
             }
 
 
             //判断右上角细胞状态
-            temp = matrixColumn - 1;
-            if ((j <= temp) && (i  >= 1) && (matrix[i - 1][j + 1] > 0))
+            if ((i - 1 >= 0) && (j + 1 < matrixColumn) && (matrix[i - 1][j + 1] == 1))
+
             {
                 nAliveCnt++;
             }
 
             //判断左细胞状态
-            if ((j >= 1) && (matrix[i][j - 1] > 0))
+            if ((j - 1 >= 0) && (matrix[i][j - 1] == 1))
+
             {
                 nAliveCnt++;
             }
 
             //判断右细胞状态
-            temp = matrixColumn - 1;
-            if ((j <= temp) && (matrix[i][j + 1] > 0))
+  
+            if ((j +1 < matrixColumn) && (matrix[i][j + 1] == 1))
             {
                 nAliveCnt++;
             }
 
             //判断左下角细胞状态
-            temp = matrixRow - 1;
-            if ((i <= temp) && (j >= 1) && (matrix[i - 1][j - 1] > 0))
+            if ((i + 1 < matrixRow) && (j - 1 >= 0) && (matrix[i + 1][j - 1] == 1))
+            if(matrix[i + 1][j - 1 ] ==1)
             {
                 nAliveCnt++;
             }
 
             // 判断下方细胞状态
-            temp = matrixRow - 1;
-            if ((i <= temp)  && (matrix[i + 1][j] > 0))
+            if ((i + 1 < matrixRow)  && (matrix[i + 1][j] == 1))
             {
                 nAliveCnt++;
             }
 
             // 判断右下角细胞状态
-            temp = matrixRow - 1;
-            anotherTemp = matrixColumn - 1;
-            if ((i <= temp) && (j <= anotherTemp) && (matrix[i + 1][j + 1] > 0))
+
+            if (( i + 1 < matrixRow) && ( j + 1 <matrixColumn) && (matrix[i + 1][j + 1] == 1))
             {
                 nAliveCnt++;
             }
 
+            // console.log(nAliveCnt);
+            
+            // 判断细胞下一代死活情况
 
-            console.log(nAliveCnt);
+            //Live
+            if (matrix[i][j] == 1)
+            {
+                if ((nAliveCnt == 2) || (nAliveCnt == 3))
+                {
+                    copyMatrix[i][j] = 1;
+                }
+                else
+                {
+                    copyMatrix[i][j] = 0;
+                    nLive--;
+                }
+            }
 
-            // // 判断细胞下一代死活情况
-            // if (matrix[i][j] == 1)
-            // {
-            //     if ((nAliveCnt == 2) || (nAliveCnt == 3))
-            //     {
-            //         copyMatrix[i][j] = 1;
-            //     }
-            //     else
-            //     {
-            //         copyMatrix[i][j] = 0;
-            //         nLive--;
-            //     }
-            // }
-
-
-            // if (matrix[i][j] == 0)
-            // {
-            //     if (nAliveCnt == 3)
-            //     {
-            //         copyMatrix[i][j] = 1;
-            //         nLive++;
-            //     }
-            // }
+            //Dead
+            if (matrix[i][j] == 0)
+            {
+                if (nAliveCnt == 3)
+                {
+                    copyMatrix[i][j] = 1;
+                    nLive++;
+                }
+            }
 
     
         }
 
     }       
+
+    
+
+    
         
         
-    // for (k = 0; k < matrixRow; k++)
+    // for (i = 0; i < matrixRow; i++)
     // {
-    //     for (p = 0; p < matrixColumn; p++)
+    //     for (j = 0; j < matrixColumn; j++)
     //     {
-    //         matrix[k][p] = copyMatrix[k][p];
+    //         matrix[i][j] = copyMatrix[i][j];
     //     }
     // }
 
 }
 
 var matrix = [
-    [1, 1, 1],
-    [0, 0, 0],
-    [0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
 gameOfLife(matrix);
