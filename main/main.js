30452d7cc9357dadc6759e215fad86cf4677a44f
Adam Cai <caiyijias@outlook.com>
2019-06-15 17:52:56 +0800

    finish aliveAroung count

--- main/main.js
+++ main/main.js
@@ -1,19 +1,22 @@
+var lifeTimer = 0;
+
 function gameOfLife(matrix) {
-    var i = 0, j = 0;
+    var i = 0,
+        j = 0;
     var nLive = 0;
     var nAliveCnt = 0;
     var nContinue = 0;
-     
+
     var matrixRow = matrix.length;
     var matrixColumn = matrix[0].length;
 
     var copyMatrix = new Array();
-    for (var k = 0; k < matrixRow; k++){
+    for (var k = 0; k < matrixRow; k++) {
         copyMatrix[k] = new Array();
 
-        for (var p = 0; p < matrixColumn; p++){
+        for (var p = 0; p < matrixColumn; p++) {
             if (matrix[k][p] == 1) {
-                nLive = nLive+1;
+                nLive = nLive + 1;
             }
             copyMatrix[k][p] = matrix[k][p];
         }
@@ -22,76 +25,60 @@ function gameOfLife(matrix) {
     // console.log(copyMatrix);
     // console.log(nLive);
 
-    while (1)
-    {
-        for (i = 0; i < matrixRow; i++)
-        {
-            for (j = 0; j < matrixColumn; j++)
-            {
-                if ((i - 1 > 0) && (j - 1 > 0) && (matrix[i - 1][j - 1] > 0))
-                    {
+    while (1) {
+        for (i = 0; i < matrixRow; i++) {
+            for (j = 0; j < matrixColumn; j++) {
+                if ((i - 1 > 0) && (j - 1 > 0) && (matrix[i - 1][j - 1] > 0)) {
                     nAliveCnt++;
                 }
-                if ((i - 1 > 0) && (matrix[i - 1][j] > 0))
-                {
+                if ((i - 1 > 0) && (matrix[i - 1][j] > 0)) {
                     nAliveCnt++;
                 }
-                if ((j + 1 < matrixColumn) && (i - 1 > 0) && (matrix[i - 1][j + 1] > 0))
-                {
+                if ((j + 1 < matrixColumn) && (i - 1 > 0) && (matrix[i - 1][j + 1] > 0)) {
                     nAliveCnt++;
                 }
-                if ((j - 1 > 0) && (matrix[i][j - 1] > 0))
-                {
+                if ((j - 1 > 0) && (matrix[i][j - 1] > 0)) {
                     nAliveCnt++;
                 }
-                if ((j + 1 < matrixColumn) && (matrix[i][j + 1] > 0))
-                {
+                if ((j + 1 < matrixColumn) && (matrix[i][j + 1] > 0)) {
                     nAliveCnt++;
                 }
-                if ((i + 1 < matrixRow) && (j - 1 > 0) && (matrix[i + 1][j - 1] > 0))
-                {
+                if ((i + 1 < matrixRow) && (j - 1 > 0) && (matrix[i + 1][j - 1] > 0)) {
                     nAliveCnt++;
                 }
-                if ((i + 1 < matrixRow) && (matrix[i + 1][j - 1] > 0))
-                {
+                if ((i + 1 < matrixRow) && (matrix[i + 1][j - 1] > 0)) {
                     nAliveCnt++;
                 }
-                if ((i + 1 < matrixRow) && (j + 1 > 0) && (matrix[i + 1][j - 1] > 0))
-                {
+                if ((i + 1 < matrixRow) && (j + 1 > 0) && (matrix[i + 1][j - 1] > 0)) {
                     nAliveCnt++;
                 }
             }
 
-            if (matrix[i][j] == 1)
-            {
-                if ((nAliveCnt != 2) || (nAliveCnt != 3))
-                {
+            if (matrix[i][j] == 1) {
+                if ((nAliveCnt != 2) || (nAliveCnt != 3)) {
                     copyMatrix[i][j] = 0;
-                }    
+                }
             }
 
-            if(matrix[i][j] == 0)
-            {
-                if (nAliveCnt == 3)
-                {
+            if (matrix[i][j] == 0) {
+                if (nAliveCnt == 3) {
                     copyMatrix[i][j] = 1;
                 }
-                
+
             }
-            
-        }       
-        
+
+        }
+
         for (var k = 0; k < matrixRow; k++) {
             for (var p = 0; p < matrixColumn; p++) {
-                
+
                 matrix[k][p] = copyMatrix[k][p];
             }
         }
 
-        console.log(matrxi);
+        console.log(matrix);
 
 
-        
     }
 
 
@@ -100,7 +87,7 @@ function gameOfLife(matrix) {
 
     // for (i = 0; i < 10; i++)
     // {
-        
+
     //     }
 
 }
@@ -116,9 +103,11 @@ var matrix = [
     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-]; 
+];
 
 gameOfLife(matrix);
+
+// lifeTimer = setInterval(gameOfLife(), 300);
 
 
 
---
+++ main/main1.js
@@ -0,0 +1,79 @@
+var lifes = [];
+var life;
+
+
+beginLife();
+
+function beginLife(X, Y) {
+    life = new LifeGame(X, Y);
+    life.initRandom(20);
+
+    for (var i = 0; i < 50; i++) {
+        lifes[i] = [];
+        for (var j = 0; j < 50; j++) {
+            lifes[i][j] = 0;
+        }
+    }
+
+    initRandom();
+}
+
+function LifeGame(X, Y) {
+    this.X = X;
+    this.Y = Y;
+    this.grid = [];
+    this.remainLifes = 0;
+    this.nextRemainLifes = 0;
+    this.period = 1;
+
+}
+
+//随机初始化细胞
+function initRandom(percent) {
+    this.remainLifes = 0;
+    this.period = 1;
+    for (var i = 0; i < this.X; i++) {
+        this.grid[i] = [];
+        for (var j = 0; j < this.Y; j++) {
+            if (Math.random() * 100 <= percent) {
+                this.grid[i][j] = {
+                    'state': 1,
+                    'nextState': 1
+                };
+                this.remainLifes++;
+                this.nextRemainLifes = this.remainLifes;
+            } else {
+                this.grid[i][j] = {
+                    'state': 0,
+                    'nextState': 0
+                };
+            }
+        }
+    }
+};
+
+//计算单个细胞下一回合状态
+function nextState(x, y) {
+    var aliveAround = this.aliveAround(x, y);
+    if (aliveAround >= 4) {
+        return 0;
+    } else if (aliveAround === 3) {
+        return 1;
+    } else if (aliveAround >= 2) {
+        return this.grid[x][y].nextState;
+    } else {
+        return 0;
+    }
+};
+
+//计算细胞周围的存活细胞数量
+function aliveAround(x, y) {
+    return this.grid[this.mapX(x - 1)][this.mapY(y - 1)].state +
+        this.grid[this.mapX(x - 1)][y].state +
+        this.grid[this.mapX(x - 1)][this.mapY(y + 1)].state +
+        this.grid[x][this.mapY(y - 1)].state +
+        this.grid[x][this.mapY(y + 1)].state +
+        this.grid[this.mapX(x + 1)][this.mapY(y - 1)].state +
+        this.grid[this.mapX(x + 1)][y].state +
+        this.grid[this.mapX(x + 1)][this.mapY(y + 1)].state;
+};
