dc2dbe71952fe5ef0c7a74fcce3b08c3f07be8f9
Leslie Wong <79917148leslie@gmail.com>
2019-06-16 09:54:55 +0800

    （黄崇鹏）正确核心逻辑及分布版本

--- main/main.js
+++ main/main.js
@@ -1,90 +1,25 @@
-<<<<<<< HEAD
-module.exports = function main(matrix) {
-    var i = 0, j = 0;
-=======
-<<<<<<< HEAD
-var lifeTimer = 0;
-
-=======
-// module.exports = function
->>>>>>> b252c5bfa7fa3fbe03b1d4b26cc8b54a621c088f
 function gameOfLife(matrix) {
-    var i = 0,
-        j = 0;
->>>>>>> ef8037b70b7755aac692b5fe2b1ac2ffdf57373c
+    var i = 0, j = 0;
     var nLive = 0;
     var nAliveCnt = 0;
-<<<<<<< HEAD
     var nContinue = 0;
 
-=======
-    // var nContinue = 0;
-     
->>>>>>> b252c5bfa7fa3fbe03b1d4b26cc8b54a621c088f
     var matrixRow = matrix.length;
     var matrixColumn = matrix[0].length;
 
     var copyMatrix = new Array();
-<<<<<<< HEAD
-    for (var k = 0; k < matrixRow; k++) {
-        copyMatrix[k] = new Array();
-
-        for (var p = 0; p < matrixColumn; p++) {
-            if (matrix[k][p] == 1) {
-                nLive = nLive + 1;
-=======
     for (i = 0; i < matrixRow; i++){
         copyMatrix[i] = new Array();
 
         for (j = 0; j < matrixColumn; j++){
             if (matrix[i][j] == 1) {
                 nLive = nLive+1;
->>>>>>> b252c5bfa7fa3fbe03b1d4b26cc8b54a621c088f
             }
             copyMatrix[i][j] = matrix[i][j];
         }
     }
 
 
-<<<<<<< HEAD
-    while (1) {
-        for (i = 0; i < matrixRow; i++) {
-            for (j = 0; j < matrixColumn; j++) {
-                if ((i - 1 > 0) && (j - 1 > 0) && (matrix[i - 1][j - 1] > 0)) {
-                    nAliveCnt++;
-                }
-                if ((i - 1 > 0) && (matrix[i - 1][j] > 0)) {
-                    nAliveCnt++;
-                }
-                if ((j + 1 < matrixColumn) && (i - 1 > 0) && (matrix[i - 1][j + 1] > 0)) {
-                    nAliveCnt++;
-                }
-                if ((j - 1 > 0) && (matrix[i][j - 1] > 0)) {
-                    nAliveCnt++;
-                }
-                if ((j + 1 < matrixColumn) && (matrix[i][j + 1] > 0)) {
-                    nAliveCnt++;
-                }
-                if ((i + 1 < matrixRow) && (j - 1 > 0) && (matrix[i + 1][j - 1] > 0)) {
-                    nAliveCnt++;
-                }
-                if ((i + 1 < matrixRow) && (matrix[i + 1][j - 1] > 0)) {
-                    nAliveCnt++;
-                }
-                if ((i + 1 < matrixRow) && (j + 1 > 0) && (matrix[i + 1][j - 1] > 0)) {
-                    nAliveCnt++;
-                }
-            }
-
-            if (matrix[i][j] == 1) {
-                if ((nAliveCnt != 2) || (nAliveCnt != 3)) {
-                    copyMatrix[i][j] = 0;
-                }
-            }
-
-            if (matrix[i][j] == 0) {
-                if (nAliveCnt == 3) {
-=======
     var temp = 0;
     var anotherTemp = 0;
 
@@ -174,11 +109,9 @@ function gameOfLife(matrix) {
             {
                 if (nAliveCnt == 3)
                 {
->>>>>>> b252c5bfa7fa3fbe03b1d4b26cc8b54a621c088f
                     copyMatrix[i][j] = 1;
                     nLive++;
                 }
-<<<<<<< HEAD
 
             }
 
@@ -188,34 +121,14 @@ function gameOfLife(matrix) {
             for (var p = 0; p < matrixColumn; p++) {
 
                 matrix[k][p] = copyMatrix[k][p];
-=======
->>>>>>> b252c5bfa7fa3fbe03b1d4b26cc8b54a621c088f
             }
 
     
         }
 
-<<<<<<< HEAD
         console.log(matrix);
-=======
-    }       
->>>>>>> b252c5bfa7fa3fbe03b1d4b26cc8b54a621c088f
-
-    // console.log(copyMatrix);
-
-<<<<<<< HEAD
-    }
 
 
-
-
-
-    // for (i = 0; i < 10; i++)
-    // {
-
-    //     }
-=======
-    
         
         
     for (i = 0; i < matrixRow; i++)
@@ -225,16 +138,6 @@ function gameOfLife(matrix) {
             matrix[i][j] = copyMatrix[i][j];
         }
     }
-
-<<<<<<< HEAD
-    
-=======
-    // console.log(m
->>>>>>> b252c5bfa7fa3fbe03b1d4b26cc8b54a621c088f
-
->>>>>>> ef8037b70b7755aac692b5fe2b1ac2ffdf57373c
-}
-
 
 // lifeTimer = setInterval(gameOfLife(), 300);
 
