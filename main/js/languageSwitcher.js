0020ea3b8724b14a2eeb7781d926c8a3f034d8ac
Leslie Wong <79917148leslie@gmail.com>
2020-02-23 09:01:23 +0800

    refactor translation logic of start button

--- main/js/languageSwitcher.js
+++ main/js/languageSwitcher.js
@@ -4,9 +4,19 @@ $(document).ready(function() {
     if ($("#selector").hasClass("cn")) {
       $("#selector").attr("class", "selector en");
       $("#resetButton").attr("value", "Reset");
-      $("#startButton").attr("value", "Start");
       $("#stopButton").attr("value", "Stop");
       $("#rateButton").attr("value", "Rate");
+      switch ($("#startButton").val()) {
+        case "开始":
+          $("#startButton").attr("value", "Start");
+          break;
+        case "暂停":
+          $("#startButton").attr("value", "Pause");
+          break;
+        case "继续":
+          $("#startButton").attr("value", "Continue");
+          break;
+      }
       //   console.log($("#originalNumberText").text());
       $("#originalNumberText").text("Original Number: ");
       $("#remainLifesText").text("Remaining Lives: ");
@@ -14,9 +24,19 @@ $(document).ready(function() {
     } else {
       $("#selector").attr("class", "selector cn");
       $("#resetButton").attr("value", "重置");
-      $("#startButton").attr("value", "开始");
       $("#stopButton").attr("value", "停止");
       $("#rateButton").attr("value", "速度");
+      switch ($("#startButton").val()) {
+        case "Start":
+          $("#startButton").attr("value", "开始");
+          break;
+        case "Pause":
+          $("#startButton").attr("value", "暂停");
+          break;
+        case "Continue":
+          $("#startButton").attr("value", "继续");
+          break;
+      }
       $("#originalNumberText").text("初始数量： ");
       $("#remainLifesText").text("剩余生命：");
       $("#evolutionTimesText").text("进化次数：");
