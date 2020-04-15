f20de9024f4152aa1661643c29d488068fe4a0aa
Leslie Wong <79917148leslie@gmail.com>
2020-04-16 03:45:02 +0800

    restructure to deploy to Heroku with node

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const main = require('./main/main');
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-main();
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.sendfile("views/index.html"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
