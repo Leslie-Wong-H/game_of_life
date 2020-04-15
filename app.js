05783266fd64a837150f506a9bc697a308dce6b9
Leslie Wong <79917148leslie@gmail.com>
2020-04-16 03:59:08 +0800

    Trigger Heroku deploy after enabling collectstatic

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
+
 express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
