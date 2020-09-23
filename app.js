9dfcb8ed2cd56eb4aefe9f455afc3f0d6e807e1c
Leslie Wong <79917148leslie@gmail.com>
2020-09-23 23:14:36 +0800

    :sparkles: feat: access-control-allow-origin for *

--- app.js
+++ app.js
@@ -31,6 +31,14 @@ const randomPatternQuery = async (expressRes) => {
 };
 
 express()
+  .use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
   .use(sslRedirect())
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
