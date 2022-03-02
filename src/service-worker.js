20282ce2ed41b66b3c24a91c8551054caeaecf11
Leslie Wong <79917148leslie@gmail.com>
2022-03-03 00:54:43 +0800

    :wrench: chore: diss sw more

--- src/service-worker.js
+++ src/service-worker.js
@@ -15,9 +15,9 @@ self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
       // Cache hit - return response
-      if (response) {
-        return response;
-      }
+      // if (response) {
+      //   return response;
+      // }
       return fetch(event.request);
     })
   );
