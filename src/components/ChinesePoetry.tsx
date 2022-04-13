25674f7213339680748326ee6bb09c10ac92326f
Leslie Wong <79917148leslie@gmail.com>
2022-04-13 22:35:27 +0800

    :hammer: refactor: ChinesePoetry js to tsx

--- src/components/ChinesePoetry.tsx
+++ src/components/ChinesePoetry.tsx
@@ -1,8 +1,8 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, FunctionComponent } from "react";
 import { CSSTransition } from "react-transition-group";
 import LanguageContext from "./LanguageContext";
 
-const ChinesePoetry = () => {
+const ChinesePoetry: FunctionComponent = () => {
   const [poetry, setPoetry] = useState([""]);
   const [store, setStore] = useState("");
   const [inProp, setInProp] = useState(false);
@@ -11,27 +11,26 @@ const ChinesePoetry = () => {
   // Detect language change
   useEffect(() => {
     const pattern = /\w/;
-    const rawData = store;
+    const rawData:string = store;
     if (!rawData) return;
     let startIndex = Number(rawData.slice(0, 1));
     let objIndex = 1;
     // when index is bigger than 9
-    if (rawData.slice(1, 2).match(pattern)) {
-      startIndex = Number(startIndex + rawData.slice(1, 2));
+    if (pattern.exec(rawData.slice(1, 2))) {
+      startIndex = Number(startIndex + (+rawData.slice(1, 2)));
       objIndex = 2;
     }
-    const data = JSON.parse(rawData.slice(objIndex, rawData.length));
-    const langMap = {
+    const data = JSON.parse(rawData.slice(objIndex, rawData.length)) as ChinesePoetryResponse;
+    const langMap:LanguageMap = {
       en: "English",
       cn: "Chinese",
     };
-    const sentenceOne = data[langMap[lang]].content[startIndex];
-    const sentenceTwo = data[langMap[lang]].content[startIndex + 1];
+    const poetry = data[langMap[lang]]
+    const sentenceOne = poetry.content[startIndex];
+    const sentenceTwo = poetry.content[startIndex + 1];
     let detail;
     if (
-      sentenceOne
-        .slice(sentenceOne.length - 1, sentenceOne.length)
-        .match(pattern)
+      pattern.exec(sentenceOne.slice(sentenceOne.length - 1, sentenceOne.length))
     ) {
       detail = sentenceOne + "; " + sentenceTwo;
     } else {
@@ -44,26 +43,32 @@ const ChinesePoetry = () => {
     setTimeout(() => {
       setInProp(false);
     }, 500);
+  // eslint-disable-next-line
   }, [lang]);
 
   // Initialize poetry data
   useEffect(() => {
-    requestPoetry();
-  }, [""]);
+    void requestPoetry();
+  }, []);
 
   async function requestPoetry() {
     const res = await fetch("https://playgameoflife.live/tang.json");
     console.time("requestPoetry");
-    const data = await res.json();
+    const data = await res.json() as ChinesePoetryResponse;
     const len = data.English.content.length;
     // take even index except the last one
     const startIndex = Math.floor(Math.random() * (len / 2)) * 2;
     // display sentences in responsive language
-    let sentenceOne;
-    let sentenceTwo;
+    let sentenceOne:string;
+    let sentenceTwo:string;
     // Diss langContext for latency in closure
     // if (lang === "cn") {
-    if (document.querySelector("#selector").className.endsWith("en")) {
+    const SelectorHTMLElement = document.querySelector<HTMLElement>("#selector") 
+    let className = ""
+    if (SelectorHTMLElement !== null) {
+      className = SelectorHTMLElement.className 
+    }
+    if (className.endsWith("en")) {
       sentenceOne = data.English.content[startIndex];
       sentenceTwo = data.English.content[startIndex + 1];
     } else {
@@ -73,9 +78,8 @@ const ChinesePoetry = () => {
     let detail;
     const pattern = /\w/;
     if (
-      sentenceOne
-        .slice(sentenceOne.length - 1, sentenceOne.length)
-        .match(pattern)
+      pattern.exec(sentenceOne
+        .slice(sentenceOne.length - 1, sentenceOne.length))
     ) {
       detail = sentenceOne + "; " + sentenceTwo;
     } else {
@@ -87,7 +91,7 @@ const ChinesePoetry = () => {
     setPoetry([detail]);
 
     // store data for languageSwitcher
-    const stringifiedData = startIndex + JSON.stringify(data);
+    const stringifiedData = `${startIndex}${JSON.stringify(data)}`;
     setStore(stringifiedData);
 
     // trick to fulfill the languageSwitcher's poetry animation
@@ -101,6 +105,7 @@ const ChinesePoetry = () => {
       id="poetry-data-display"
       className="poetry-data-display"
       onClick={requestPoetry}
+      aria-hidden="true"
     >
       <CSSTransition in={inProp} timeout={500} classNames="poetry">
         <div id="poetry-data-box" className="poetry-data-box">
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -12,3 +12,32 @@ interface RuleDescriptionModalProps {
   show: boolean;
   onHide: () => void;
 }
+
+type Language = "en" | "cn";
+
+interface LanguageMap {
+  en: "English";
+  cn: "Chinese";
+}
+
+interface ChinesePoetry {
+  title: string;
+  dynasty: string;
+  author: string;
+  content: string[];
+}
+
+interface EnglishPoetry {
+  title: string;
+  dynasty: string;
+  author: string;
+  content: string[];
+}
+
+interface ChinesePoetryResponse {
+  _id: string;
+  index: string;
+  Chinese: ChinesePoetry;
+  English: EnglishPoetry;
+  tags: string[];
+}
